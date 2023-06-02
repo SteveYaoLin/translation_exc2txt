8029ea48eabe9a9a29085d44c0c3e7918066e141
SteveYaoLin <yaolin0616@163.com>
2023-06-02 18:49:14 +0800

    保存文档命名，依次加1

--- translation_exc2txt.py
+++ translation_exc2txt.py
@@ -8,15 +8,23 @@ def extract_row_elements(filepath, sheetname, row_index, output_filename):
     sheet = workbook[sheetname]
     # 获取指定行的所有单元格
     row = sheet[row_index]
-    # 创建文本文件路径
-    output_filepath = os.path.join(os.path.dirname(filepath), output_filename)
+    # 获取Python脚本所在的目录
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # 构建输出文件路径
+    output_filepath = os.path.join(script_dir, output_filename)
+    # 检查是否存在同名文件，如果存在，则递增数字命名
+    count = 1
+    while os.path.exists(output_filepath):
+        output_filename = f"output{count}.txt"
+        output_filepath = os.path.join(script_dir, output_filename)
+        count += 1
     # 创建文本文件
     with open(output_filepath, 'w') as output_file:
         # 逐个写入每个单元格的值，每个值占据一行
         for cell in row:
             output_file.write(str(cell.value) + '\n')
 
-    print("提取完成！")
+    print(f"提取完成！输出文件为: {output_filepath}")
 
 # 获取用户输入的Excel文件路径
 excel_filepath = input("请输入Excel文件路径：")
